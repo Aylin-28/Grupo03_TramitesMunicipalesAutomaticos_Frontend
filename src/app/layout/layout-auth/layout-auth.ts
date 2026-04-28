035cc482e246b568728127061ca1be9b77357c83
LitanoMarceloAngeloAlexis <U23210902@utp.edu.pe>
2026-04-27 23:35:22 -0500

    Fix order

--- src/app/layout/layout-auth/layout-auth.ts
+++ src/app/layout/layout-auth/layout-auth.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLanding } from '../../components/landing/header-landing/header-landing';
+import { FooterLanding } from '../../components/landing/footer-landing/footer-landing';
 
 @Component({
   selector: 'app-layout-auth',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLanding, FooterLanding],
   templateUrl: './layout-auth.html',
   styleUrl: './layout-auth.css',
 })
-export class LayoutAuth {
-
-}
+export class LayoutAuth { }
