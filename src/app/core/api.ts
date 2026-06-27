c2624ffadb2764c36a57c5c2ccc326f3cfa550de
Aylin-28 <U23231775@utp.edu.pe>
2026-06-27 09:34:10 -0500

    History html

--- src/app/core/api.ts
+++ src/app/core/api.ts
@@ -1,6 +1,6 @@
 // Para las apis
 
 export const BASE_URL = 'http://localhost:8000/api/v1'
-export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6ImpvaG5kb2VAZXhhbXBsZS5jb20iLCJleHAiOjE3ODIyNzQ2NzR9.rkKm3uKGy81O6pisS3nbEGEPEwo0Lxzw2P_PMEZUD10'
+export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6ImpvaG5kb2VAZXhhbXBsZS5jb20iLCJleHAiOjE3ODI1NzUwMjl9.wBlEnzPAqIxCY5SMKCpBOFi1zLetKMVfTugLqasnfrU'
 
 // CAMBIAR LA UBICACIÓN DEL TOKEN, SOLO PRUEBAS 
