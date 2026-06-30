039e775a0c3bbd3ef472ecd3c2d0882191a5bb6d
YasinRomero <U23220351@utp.edu.pe>
2026-06-30 07:32:45 -0500

    Actualizando para despliege

--- src/app/core/api.ts
+++ src/app/core/api.ts
@@ -1,6 +1,10 @@
 // Para las apis
 
-export const BASE_URL = 'http://localhost:8000/api/v1'
+const IS_PRODUCTION = true;
+
+const PRODUCTION_URL = 'https://tramitesmunicipales.onrender.com/api/v1';
+const DEVELOPMENT_URL = 'http://localhost:8000/api/v1'
+export const BASE_URL = IS_PRODUCTION ? PRODUCTION_URL : DEVELOPMENT_URL;
 
 export const BASE_AUTH_URL = `${BASE_URL}/auth`;
 export const BASE_IA_URL = `${BASE_URL}/ai/ask`;
