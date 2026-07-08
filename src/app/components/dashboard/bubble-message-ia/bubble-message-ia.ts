d78f559bdf1291ead7d91167299d70109994902f
LitanoMarceloAngeloAlexis <U23210902@utp.edu.pe>
2026-07-07 22:15:07 -0500

    Better Steps | Sesion | Confetti

--- src/app/components/dashboard/bubble-message-ia/bubble-message-ia.ts
+++ src/app/components/dashboard/bubble-message-ia/bubble-message-ia.ts
@@ -3,6 +3,7 @@ import { Button } from '../../ui/button/button';
 import { IconComponent } from '../../ui/icon-component/icon-component';
 import { CardDownloadIA } from '../card-download-ia/card-download-ia';
 import { CardInputFieldIA } from '../card-input-field-ia/card-input-field-ia';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-bubble-message-ia',
@@ -17,4 +18,46 @@ export class BubbleMessageIA {
   quickReplies = input<QuickReplyItem[]>([]);
   inputFields = input<InputFieldCardItem[]>([]);
   steps = input<string[]>([]);
+  checkedSteps = new Set<number>();
+
+  celebrate() {
+    confetti({
+      particleCount: 150,
+      spread: 90,
+      origin: { y: 0.7 }
+    });
+  }
+
+  toggleStep(index: number, event: Event) {
+
+    const checked = (event.target as HTMLInputElement).checked;
+
+    if (checked) {
+      this.checkedSteps.add(index);
+    } else {
+      this.checkedSteps.delete(index);
+    }
+
+    this.checkCompletion();
+  }
+
+
+  private celebrated = false;
+
+  checkCompletion() {
+
+    const totalSteps = this.steps().length;
+
+    if (
+      this.checkedSteps.size === totalSteps &&
+      !this.celebrated
+    ) {
+
+      this.celebrated = true;
+
+      this.celebrate();
+
+    }
+
+  }
 }
